dad859a0475ff777bd9f1d4480bde7e5816f73ef
rahmouni oussama <ousrah@hotmail.com>
2022-09-08 10:19:04 +0100

    etude de cas librairie

--- Bases de données/1 - Test de positionnement/script.sql
+++ Bases de données/1 - Test de positionnement/script.sql
@@ -1,12 +1,12 @@
 Create database location ;
 use location ;
-drop table if exists type_bien;
-drop table if exists ville;
-drop table if exists quartier;
+
 drop table if exists contrat;
 drop table if exists bien;
 drop table if exists client;
-
+drop table if exists quartier;
+drop table if exists type_bien;
+drop table if exists ville;
 
 
 
@@ -14,14 +14,14 @@ create table type_bien(
 	id int auto_increment not null,
 	libelle varchar(15),
 	constraint pk_type_bien primary key (id)
-) engine = innodb;
+) ;
 
 
 create table ville(
 	id int auto_increment not null,
 	libelle varchar(15),
 	constraint pk_ville primary key (id)
-) engine = innodb;
+) ;
 
 
 
@@ -31,15 +31,15 @@ create table ville(
 	constraint pk_quartier primary key (id),
 	id_ville int, 
    constraint fk_quartier_ville foreign key (id_ville) references ville(id)
-) engine = innodb;
+);
 
  create table client(
 	cin varchar(15) primary key,
 	nom varchar(15),
 	prenom varchar(15),
 	telephone varchar(10),
-	adresse varchar(50),
-) engine = innodb;
+	adresse varchar(50)
+) ;
 
  create table bien(
 	reference int auto_increment primary key,
@@ -53,7 +53,7 @@ create table ville(
 	constraint fk_bien_client foreign key (cin) references client(cin),
 	constraint fk_bien_type foreign key (id_type_bien) references type_bien(id),
 	constraint fk_bien_quartier foreign key (id_quartier) references quartier(id)
-) engine = innodb;
+) ;
 
  create table contrat(
 	date_creation date,
@@ -66,7 +66,7 @@ create table ville(
    constraint fk_contrat_client foreign key (cin) references client(cin),
    constraint fk_contrat_bien foreign key  (reference) references bien (reference),
 	constraint pk_contrat primary key (cin, reference)
-) engine = innodb;
+);
 
 
 
